6d7863b11b00f6dc7abac234258a991dbd20d026
Leonardo <leonardolivelopes2@gmail.com>
2021-10-03 19:58:50 -0300

    arrumei texto dos botões

--- script/temporizador.js
+++ script/temporizador.js
@@ -1,8 +1,3 @@
-function addTime() {
-    document.querySelector(".wrapper-modal").classList.toggle("visible")
-}
-
-
 const hour  = document.querySelector("#hour")
 const minutes = document.querySelector("#minutes")
 const seconds  = document.querySelector("#seconds")
@@ -13,8 +8,12 @@ const minutesSelect = document.querySelector(".selectMinutes")
 const hourSelect = document.querySelector(".selectHour")
 
 let min = 0
-let h = 0
-let sec = 0
+let hr = 0
+let sec = 0 
+
+let interval
+
+
 
 const Utils = {
 
@@ -38,16 +37,105 @@ const Utils = {
     
     }, 
 
-    selected(select, element, valueUpdate) {
-        let option = select.options[select.selectedIndex]
+    clearFields() {
+        sec = 0
+        min = 0
+        hr = 0
+        hour.innerHTML = "0" + hr
+        minutes.innerHTML = "0" + min
+        seconds.innerHTML = "0" + sec
+    },
 
-        element.innerHTML = option.value
+    validatDisabled(el, el2) { 
+        el.hasAttribute("disabled") ? el.removeAttribute("disabled") : el.setAttribute("disabled", true)
+    
+        el2.hasAttribute("disabled") ? el2.removeAttribute("disabled") : el2.setAttribute("disabled", true)
+    }
+    
+}
+
+
+const finishTime = () => {
+    DOM.stop()
+
+    const body = document.querySelector("body")    
+    
+    const modalStop = document.querySelector(".modal-stop")
+            modalStop.classList.add("on")
+    
+    body.innerHTML = ""
+    body.appendChild(modalStop)
+}
+
+const App = {
+    selected(select) {
+
+        
+        select
+            .addEventListener("change", () => {
+
+                let option = select.options[select.selectedIndex].value
+
+                if(select == secondsSelect) {
+                    sec = Number(option)
+                }
+                if(select == minutesSelect) {
+                    min = Number(option)
+                }
+                if(select == hourSelect) {
+                    hr = Number(option)
+                }
+
+            })
+ 
+    },
+
+    startTemp() {
+
+        if(hr < 10 || min < 10 || sec < 10) {
+    
+    
+            if(hr < 10) {
+                hour.innerHTML = "0" + hr
+            } else {
+                hour.innerHTML = hr
+            }
+    
+            if(min < 10) {
+                minutes.innerHTML = "0" + min  
+            } else {
+                minutes.innerHTML = min
+            }
+    
+            if(sec < 10) {
+                seconds.innerHTML = "0" + sec  
+            } 
+            else {
+                seconds.innerHTML = sec
+            }
+        }
+
+        if(sec <= 60 && sec > 0){
+            sec--
+        } else if(min <= 60 && min > 0) {
+            min--
+            sec = 59 
+        } else if(hr > 0){
+            hr--
+            min = 59
+            sec = 59 
+        } else if(sec == 0 && min == 0 && sec == 0) {
+            finishTime()
+        }
+        
+    
+        
     }
 }
 
 
 
-const Start = {
+const Build = {
     BuildValuesOption() {
 
         Utils.gerarOption(secondsSelect)
@@ -58,26 +146,73 @@ const Start = {
 
     UpdateValuesCards() {
 
-        secondsSelect
-            .addEventListener("change", () => Utils.selected(secondsSelect, seconds, sec))
-        minutesSelect
-            .addEventListener("change", () => Utils.selected(minutesSelect, minutes))
-        hourSelect
-            .addEventListener("change", () => Utils.selected(hourSelect, hour))
+        App.selected(secondsSelect)
+        App.selected(minutesSelect)
+        App.selected(hourSelect)
 
     }
 }
 
-// audio alarme
-// function audio(event) {
-//     event.preventDefault()
+
+Build.BuildValuesOption()
+Build.UpdateValuesCards()
+
+
+let validatText = true
+
+const button =  document.querySelector(".start")
+const buttonReset =  document.querySelector(".reset")
+
+const buttonStop = document.querySelector(".stop")
+buttonStop.setAttribute("disabled", true)
+
+
+const DOM = {    
+
+    toggleModal() {
+        document.querySelector(".wrapper-modal").classList.toggle("visible")
+    },
     
+    addTime() {
+        
 
-//     const alarme = new Audio
-//     alarme.src = "../audio/mixkit-warning-alarm-buzzer-991.wav"
+            DOM.toggleModal()
+        
+    },
 
-//     alarme.play()
-// }
+    start(event) {
+        event.preventDefault()
+        if(button.innerText == "Continuar") {
+            button.innerText = "+Tempo"
+        }
 
-Start.BuildValuesOption()
-Start.UpdateValuesCards()
+        buttonReset.setAttribute("disabled", true)
+        interval = setInterval(App.startTemp, 100)
+        DOM.addTime()
+        Utils.validatDisabled(button, buttonStop)
+    },
+
+    stop() {
+        clearInterval(interval);
+        if(button.innerText == "+Tempo") {
+            button.innerText = "Continuar"
+        }
+        Utils.validatDisabled(button, buttonStop)
+        buttonReset.removeAttribute("disabled")
+    },
+
+
+    reset() {
+        clearInterval(interval);
+        Utils.clearFields()
+        Utils.validatDisabled(button, buttonStop)
+        DOM.addTime()
+    },
+
+
+    toggle() {
+        document.querySelector('.menu-section').classList.toggle('on')
+        document.querySelector('nav').classList.toggle('visible')
+        document.querySelector('body').style.overflow = 'hidden'
+    },
+}
